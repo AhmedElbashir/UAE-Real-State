926870e7ea03dd40ad11a696ad159df3469c8032
AhmedElbashir <107240463+AhmedElbashir@users.noreply.github.com>
2024-08-26 01:19:39 +0400

    Update realestate_last_app.py

--- realestate_last_app.py
+++ realestate_last_app.py
@@ -14,16 +14,6 @@ import numpy as np
 
 # Load the trained model
 realestate_model = pickle.load(open('best_gb_reg.sav', 'rb'))
-# Ensure the loaded model has a predict method
-    if not hasattr(realestate_model, 'predict'):
-        st.error(f"The loaded object is not a model or does not have a 'predict' method. Loaded object type: {type(model)}")
-        st.stop()
-except FileNotFoundError:
-    st.error(f"Model file not found: {realestate_model}")
-    st.stop()
-except Exception as e:
-    st.error(f"An error occurred while loading the model: {e}")
-    st.stop()
 
 
 # Load feature names or assume you have them saved
